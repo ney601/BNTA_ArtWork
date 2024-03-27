38879fd2a9095bbc04cdfcc77bed952a02e722b5
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-03-27 14:29:53 +0000

    Fetch department data

--- src/containers/ArtworkContainer.js
+++ src/containers/ArtworkContainer.js
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const ArtworkContainer = () => {
+
+    const [artworks, setArtworks] = useState([]);
+    const [departments, setDepartments] = useState([]);
+
+    const fetchDepartments = async () => {
+        const response = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments");
+        const jsonData = await response.json();
+        setDepartments(jsonData);
+    }
+
+    useEffect(() => {
+        fetchDepartments();
+    }, []);
+
+
     return ( 
         <>
           <h2>Container</h2>
+          {JSON.stringify(departments)}
         </>
     );
 }
